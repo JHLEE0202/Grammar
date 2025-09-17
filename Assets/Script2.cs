95337d0236a76835e3f80bafc5df30a1105601a1
JHLEE0202 <202120662@sangmyung.kr>
2025-09-17 13:55:58 +0900

    실습3

--- Assets/Script2.cs
+++ Assets/Script2.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Script2 : MonoBehaviour
 {
+    public int inch;
+    private float cm;
     // Start is called before the first frame update
     void Start()
     {
-        
+      cm = inch*2.54f;
+      Debug.Log(inch + "인치는" + cm + "입니다.");
     }
 
     // Update is called once per frame
