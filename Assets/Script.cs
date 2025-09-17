966e919c46e5bb65beafb0082634eae148988637
JHLEE0202 <202120662@sangmyung.kr>
2025-09-17 12:49:42 +0900

    실습

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -8,14 +8,17 @@ public class Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+       int n1 = 8;
+       int n2 = 9;
+       int answer;
+       answer = n1 + n2;
+       Debug.Log(answer);
         
-        
-       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(maxCount); 
+        
     }
 }
